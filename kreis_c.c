ae5f1082cb03a1035d02c70002905928aef38489
Sherry <119943082+HonHonX@users.noreply.github.com>
2023-01-05 09:45:38 +0100

    Create kreis_c.c

---
+++ kreis_c.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "kreisberechnung.h"
+#define PI 3.141592653589793
+
+//Hauptprogramm
+int main () {
+  /*
+  * Dieses Programm soll den Radius eines Kreises einlesen und darauf basierend dann den Flächeninhalt + Umfang berechnen und ausgeben
+  */
+  
+  //Programmtitel
+  printf("\n ****************************************** ");
+	printf("\n *   ^,,,,^   *                           * ");
+	printf("\n *  ( o x o)  * Kreisflächeninhalt+Umfang * ");
+	printf("\n *  ( 7   7)  *                           * ");
+	printf("\n *   ´´´´´´   *                ©S.Buch    * ");
+	printf("\n ****************************************** \n\n");
+  
+  //Berechnung
+  ausgabeErgebnis(eingabeZahl());
+  
+  //Ende
+	return 0;
+}
